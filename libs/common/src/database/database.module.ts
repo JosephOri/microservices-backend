5794bb21b78c843c32938ca9ab2552259f613fb8
josephOri <ori100no@gmail.com>
2025-11-08 10:53:18 +0200

    created abstract repository,schema and model for spearing repetative logic

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/microservices-backend'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
   ],
 })
 export class DatabaseModule {}
