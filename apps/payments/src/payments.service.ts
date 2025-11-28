91270193b31da30a81a8b3ad714941cdbff2680c
josephOri <ori100no@gmail.com>
2025-11-28 22:36:24 +0200

    created notifications services

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,20 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
-import { CreateChargeDto } from '@app/common';
+import { CreateChargeDto, NOTIFICATIONS_SERVICE } from '@app/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { PaymentsCreateChargeDto } from './dto/payments-create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe: Stripe;
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE)
+    private readonly notificationsService: ClientProxy,
+  ) {
     this.stripe = new Stripe(this.configService.get('STRIPE_SECRET_KEY'));
   }
-  async createCharge({ amount }: CreateChargeDto) {
+  async createCharge({ card, amount, email }: PaymentsCreateChargeDto) {
     const paymentIntent = await this.stripe.paymentIntents.create({
       payment_method: 'pm_card_visa',
       amount: amount * 100,
       confirm: true,
       currency: 'usd',
+    });
+
+    this.notificationsService.emit('notify_email', {
+      email,
     });
     return paymentIntent;
   }
--- libs/common/src/constants/services.ts
+++ libs/common/src/constants/services.ts
@@ -1,2 +1,3 @@
 export const AUTH_SERVICE = 'auth';
 export const PAYMETS_SERVICE = 'payments';
+export const NOTIFICATIONS_SERVICE = 'notifications';
